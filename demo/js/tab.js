1dabd716e2b3a980bade77b20ac8d33aeb798287
jian.che <514822235@qq.com>
2022-03-16 21:28:00 +0800

    V1

---
+++ demo/js/tab.js
@@ -0,0 +1,90 @@
+
+class Tab {
+    constructor(id) {
+        that = this;
+        this.main = document.querySelector(id);
+        this.add = this.main.querySelector('.tabadd');
+        this.ul = this.main.querySelector('.fisrstnav ul:first-child');
+        this.fsection = this.main.querySelector('.tabscon');
+        this.init();
+    }
+    init() {
+            this.updateNode();
+            // init 初始化操作让相关的元素绑定事件
+            this.add.onclick = this.addTab;
+            for (var i = 0; i < this.lis.length; i++) {
+                this.lis[i].index = i;
+                this.lis[i].onclick = this.toggleTab;
+                this.remove[i].onclick = this.removeTab;
+                this.spans[i].ondblclick = this.editTab;
+                this.sections[i].ondblclick = this.editTab;
+
+            }
+        }
+    updateNode = ()=> {
+            this.lis = this.main.querySelectorAll('li');
+            this.sections = this.main.querySelectorAll('section');
+            this.remove = this.main.querySelectorAll('.icon-guanbi');
+            this.spans = this.main.querySelectorAll('.fisrstnav li span:first-child');
+        }
+    toggleTab() {
+            that.clearClass();
+            this.className = 'liactive';
+            that.sections[this.index].className = 'conactive';
+        }
+    clearClass() {
+            for (var i = 0; i < this.lis.length; i++) {
+                this.lis[i].className = '';
+                this.sections[i].className = '';
+            }
+        }
+        // 2. 添加功能
+    addTab() {
+            that.clearClass();
+            // (1) 创建li元素和section元素 
+            var li = '<li class="liactive"><span>新选项卡</span><span class="iconfont icon-guanbi"></span></li>';
+            var section = '<section class="conactive">添加新内容：</section>';
+            // (2) 把这两个元素追加到对应的父元素里面
+            that.ul.insertAdjacentHTML('beforeend', li);
+            that.fsection.insertAdjacentHTML('beforeend', section);
+            that.init();
+        }
+        // 3. 删除功能
+    removeTab(e) {
+            e.stopPropagation(); // 阻止冒泡 防止触发li 的切换点击事件
+            var index = this.parentNode.index;
+            console.log(index);
+            // 根据索引号删除对应的li 和section   remove()方法可以直接删除指定的元素
+            that.lis[index].remove();
+            that.sections[index].remove();
+            that.init();
+            if (document.querySelector('.liactive')) return;
+            index--;
+            that.lis[index] && that.lis[index].click();
+        }
+
+}
+var that;
+class NewTab extends Tab {
+    constructor(id) {
+        super(id);
+    }
+    editTab() {
+        var str = this.innerHTML;
+        window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
+        this.innerHTML = '<input type="text" />';
+        var input = this.children[0];
+        input.value = str;
+        input.select();
+        input.onblur = function() {
+            this.parentNode.innerHTML = this.value;
+        };
+        input.onkeyup = function(e) {
+            if (e.keyCode === 13) {
+                this.blur();
+            }
+        }
+    }
+
+}
+new NewTab('#tab')
