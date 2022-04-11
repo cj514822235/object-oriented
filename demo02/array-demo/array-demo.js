bb992975ef9ae908258f3746512398017f10a784
jian.che <514822235@qq.com>
2022-04-11 21:47:48 +0800

    test

--- demo02/array-demo/array-demo.js
+++ demo02/array-demo/array-demo.js
@@ -6,5 +6,23 @@ Array.prototype.sum = function() {
     return sum;
 };
 
+const arrays = [1,2,3,5];
+
+const arraySum = (array)=>{
+   const sum =  array.reduce((prValue,cuValue,index)=> {
+       console.log(index)
+       return prValue+cuValue;
+   },3)
+   return sum
+}
+
+const arrayMap = (array) =>{
+    return array.map((num)=>num+1)
+}
+
+console.log(arrayMap(arrays));
+
+console.log(arraySum(arrays));
+
 const arr = [1,2,3];
 console.log(arr, 'call sum function: ' + arr.sum());
